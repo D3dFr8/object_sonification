4b88a425a6c303e2ee7699597ec2c5b665578134
Edvard <edvni588@student.liu.se>
2026-02-10 15:59:35 +0100

    calibration works with a good validation error. Next step: object detection

--- code/calibrate.py
+++ code/calibrate.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import glob
+import random
 import os
 
 # termination criteria
@@ -14,43 +15,50 @@ sq_size = 40
 objp[:,:2] = np.mgrid[0:13,0:9].T.reshape(-1,2) * sq_size
 
 # Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+trainobjpoints = [] # 3d point in real world space
+trainimgpoints = [] # 2d points in image plane.
 
 #dirname = os.path.dirname(__file__)
 #filename = os.path.join(dirname, '/images')
 
 #print(dirname)
 images = glob.glob('/home/pi2/Documents/exjobb/tqet33-exjobb/code/imagesForCalibVer2/*.jpg')
+random.shuffle(images)
+
+split_idx = int(len(images)*0.8)
+train_imgs = images[:split_idx] #training set
+valid_imgs = images[split_idx:] #validation set
 #print(images)
 
-for fname in images:
+for fname in train_imgs:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (13,9), None)
-    print(ret)
+    print(f'{fname}: {ret}')
 
     # If found, add object points, image points (after refining them)
     if ret == True:
-        objpoints.append(objp)
+        trainobjpoints.append(objp)
 
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners2)
+        trainimgpoints.append(corners2)
 
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (13,9), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        #cv.drawChessboardCorners(img, (13,9), corners2, ret)
+        #cv.imshow('img', img)
+        #cv.waitKey(500)
 
-cv.waitKey(1000)
+#cv.waitKey(1000)
 
-cv.destroyAllWindows()
+#cv.destroyAllWindows()
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(trainobjpoints, trainimgpoints, 
+                                gray.shape[::-1], None, None, 
+                                flags=cv.CALIB_FIX_K3) #remove k3, extraneous distortion parameter
 
-img = cv.imread('/home/pi2/Documents/exjobb/tqet33-exjobb/code/imagesForCalibVer2/image0000.jpg')
+img = cv.imread('/home/pi2/Documents/exjobb/tqet33-exjobb/code/test.jpg')
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 0, (w,h))
 
@@ -60,12 +68,47 @@ dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 # crop the image
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
-cv.imwrite('calibresult.png', dst)
+cv.imwrite('imageAfter.png', dst)
 
+#calculate re-projection (training) error
 mean_error = 0
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+for i in range(len(trainobjpoints)):
+    trainimgpoints2, _ = cv.projectPoints(trainobjpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(trainimgpoints[i], trainimgpoints2, cv.NORM_L2)/len(trainimgpoints2)
     mean_error += error
 
-print( "total error: {}".format(mean_error/len(objpoints)) )
+print( "re-projection error: {}".format(mean_error/len(trainobjpoints)) )
+
+
+#---------- validation ----------#
+total_valid_error = 0
+total_points = 0
+
+for fname in valid_imgs:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (13,9), None)
+    print(f'{fname}: {ret}')
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        #get accurate position of corners
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+
+        #calculate rotation and translation vectors for validation image
+        _, rvec, tvec = cv.solvePnP(objp, corners2, mtx, dist)
+        #use these together with previously known camera matrix to predict where the corners should be
+        projected_corners, _ = cv.projectPoints(objp, rvec, tvec, mtx, dist)
+
+        #error between predicted position of corners and actual
+        error = cv.norm(corners2, projected_corners, cv.NORM_L2)
+        total_valid_error += error
+        total_points += len(corners2)
+
+mean_error_perimg = total_valid_error/len(valid_imgs) #sum of all errors in one image (on average)
+mean_error_perpoint = total_valid_error/total_points #average error for every point
+
+print(f'Mean validation error per image: {mean_error_perimg}')
+print(f'Mean validation error per point: {mean_error_perpoint}')
