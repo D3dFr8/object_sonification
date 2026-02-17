8f55b3de2a75b77e3b00ff5db22735a911ba2cb8
Edvard <edvni588@student.liu.se>
2026-02-17 17:09:33 +0100

    tried depth, is constant for some reason. Trying to get audio working

--- code/object_localization.py
+++ code/object_localization.py
@@ -34,14 +34,15 @@ config = picam.create_preview_configuration(
 )
 picam.configure(config)
 
-#get path to model and use model
-#imx500.config(picam, model_file=model)
+audio = AudioSegment.from_mp3("soundreality-finger-snap-sound-423220.mp3")
+left_channel, right_channel = audio.split_to_mono()
+left_channel = np.array(left_channel.get_array_of_samples())
+right_channel = np.array(right_channel.get_array_of_samples())
 
 #start camera
 picam.start_preview(Preview.QTGL)
 picam.start()
 
-time.sleep(3)
 print("Camera started, precc Ctrl+C to stop")
 try:
     while True:
@@ -63,7 +64,7 @@ try:
                     x, y, w, h = boxes[i]
                     confidence = scores[i]
                     category = classes[i]
-                    #labels = get_labels()
+
                     #get exact pixel location of object:
                     width, height = 2028, 1520
 
@@ -72,17 +73,31 @@ try:
                     pixel_w = w * width
                     pixel_h = h * height
 
-                    #label = f"{labels[int(category)]} ({confidence:.2f})"
-                    #cv.putText(m.array, label, (x + 5, y + 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-                    #cv.rectangle(m.array, (x, y), (x + w, y + h), (0, 0, 255, 0))
-
                     #get coordinates of middle of object
                     u = float(pixel_x + (pixel_w/2))
                     v = float(pixel_y + (pixel_h/2))
                     w = 1.0
 
+                    #create array with pixel coordinates and convert to camera's coordinates
                     center_coords = np.array([u, v, w], dtype=np.float64)
                     camera_vec = cam_mtx_inv.dot(center_coords)
+                    camera_vec[0] = -camera_vec[0]
+                    #camera_vec[1] = -camera_vec[1]
+
+                    #convert cartesian coordinates to spherical and compute depth
+                    r = np.linalg.norm(camera_vec)
+                    azimut = 90 - (180/np.pi * np.arccos(camera_vec[1]/r))
+                    elevation = 180/np.pi * np.arctan(camera_vec[0])
+
+                    #real_w_human = 0.6
+                    #focal_len = cam_mtx[0, 0]
+                    #depth = real_w_human*focal_len/pixel_w
+
+
+                    print(f'Object of class {category} found with confidence {confidence:.2f}')
+                    #print(f'Camera vector: {camera_vec}')
+                    print(f'Azimut: {float(azimut)}, Elevation: {float(elevation)}')
+
                     #vec_2d = cv.undistortPoints(center_coords, cam_mtx, distortion)
                     
                     #vec_x = vec_2d[0,0,0]
@@ -90,23 +105,14 @@ try:
 
                     #camera_vec = np.array([vec_x, vec_y])
 
-                    #convert cartesian coordinates to spherical
-                    r = np.linalg.norm(camera_vec)
-                    theta = 90 - (180/np.pi * np.arccos(-camera_vec[1]/r))
-                    phi = 180/np.pi * np.arctan(camera_vec[0])
-
-                    print(f'Object of class {category} found with confidence {confidence:.2f} at {(u, v, w)}')
-                    print(f'Camera vector: {camera_vec}')
-                    print(f'Angles: {(phi, theta)}')
-
-                    left, right = findNearestHRIR(createPointFromSph(phi, theta, 1))
-                    audio = AudioSegment.from_mp3("soundreality-finger-snap-sound-423220.mp3")
+                    left, right = findNearestHRIR(createPointFromSph(azimut, elevation, 1))
                     
-                    #left_conv = np.convolve(left, audio)
-                    #right_conv = np.convolve(right, audio)
+                    left_conv = np.convolve(left, left_channel)
+                    right_conv = np.convolve(right, right_channel)
                     
                     #sounddevice.play()
-                    #print(f'Left: {left_conv}, Right: {right_conv}')
+                    #print(f'Left: {left}, Right: {right}')
+                    #print(f'L channel: {left_conv}, R channel: {right_conv}')
             print("-------------------------------------------")
 
         time.sleep(0.01)
