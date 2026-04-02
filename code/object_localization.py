70b6b0b8a39f83c3293f895433bc710dfcb65e7f
Edvard Nilsson <edvni588@student.liu.se>
2026-04-02 20:10:32 +0200

    color segmentation implemented for yellow. Test to come on yellow balloon

---
+++ code/object_localization.py
@@ -0,0 +1,302 @@
+from picamera2.devices.imx500 import IMX500
+from picamera2 import Picamera2, Preview
+import numpy as np
+import time
+import datatypes.point as point
+import sound.soundTools as st
+import sound.loadHrir as lH
+import sound.getHRIR as getHRIR
+import datatypes.angle as angle
+from multiprocessing import Process, Pipe
+#from datatypes.point import *
+import matplotlib.pyplot as plt
+from datatypes.chirp import Chirp
+import sys
+import os
+import cv2 as cv
+import glob
+
+"""
+Framtiden: Logga i en fil och köra med olika filter
+20 min halvtidspresentation. Målgrupp: andra studenter
+"""
+#init chirp
+sr = lH.getSamplingRate()
+chirp = Chirp(200, 400, 0.05, sr, 0.3)
+
+#----------------------------------------------#
+#-----------------AUDIO HANDLER----------------#
+#----------------------------------------------#
+def audio_process(conn, sr):
+    while True:
+        #returns True if there is data in pipeline. Block for 50 ms
+        if conn.poll():
+            try:
+                #extract latest pipeline data
+                msg = conn.recv()
+
+                #check if we want to exit
+                if msg == False:
+                    print("Audio process exiting")
+                    break
+                
+                #extract coords
+                az, el, r = msg
+
+                #drain pipeline of all older messages
+                while conn.poll():
+                    msg = conn.recv()
+                    if msg == False: return
+                    az, el, r = msg
+
+                #create target point
+                targetAz = angle.createAngleFromDegrees(az)
+                targetEl = angle.createAngleFromDegrees(el)
+                targetR = r
+                target_point = point.createPointFromSph(targetAz,targetEl,targetR)
+
+                #update length of chirp with duration scaled with distance
+                #calculate HRIR, convolve, and play sound
+                chirp.create_signal(targetR)
+                signal = chirp.get_signal()
+                
+                hL,hR = getHRIR.getHrirAtTarget(target_point, 0.1)
+                yL = st.conv(signal,hL).tolist()
+                yR = st.conv(signal,hR).tolist()
+
+                st.playSound(yL,yR,sr)
+
+            except EOFError:
+                print("End of file error, something unexpected happened")
+                break
+
+#--------------------------------------------------#
+#------------------MAIN CAMERA LOOP----------------#
+#--------------------------------------------------#
+if __name__ == "__main__":
+    localization_type = sys.argv[1]
+    path = os.path.abspath(os.getcwd())
+    
+    #load category file
+    file_path = 'categories.txt'
+    with open(file_path, 'r') as file:
+        categories = file.readlines()
+    
+    #load dimensions file
+    file_path = 'dims.txt'
+    with open(file_path, 'r') as file:
+        dims = file.readlines()
+
+    parent_conn, child_conn = Pipe()
+    p = Process(target=audio_process, args=(child_conn, sr))
+    p.start()
+
+    #get dictionary with results from the calibration
+    calib_results = np.load("calib_results.npy", allow_pickle=True)
+    #resolution for calibration images was 4056 × 3040, so we take half of that
+    cam_mtx = calib_results.item()["camera matrix"]*0.5
+    cam_mtx[2,2] = 1.0
+    distortion = calib_results.item()["distortion coeff"]
+    cam_mtx_inv = np.linalg.inv(cam_mtx)
+
+    #init camera
+    picam = Picamera2()
+    model = "/usr/share/imx500-models/imx500_network_ssd_mobilenetv2_fpnlite_320x320_pp.rpk"
+    imx500 = IMX500(model)
+
+    #configure camera with fps and resolution. Half of the OG resolution is used to ensure stable performance
+    config = picam.create_preview_configuration(
+        main={"size": (2028, 1520), "format": "XRGB8888"},
+        controls={"FrameDurationLimits": (33333, 33333)}
+    )
+    picam.configure(config)
+    #start camera
+    picam.start_preview(Preview.QTGL)
+    picam.start()
+
+    print("Camera started, precc Ctrl+C to stop")
+    try:
+        focal_lenx = cam_mtx[0,0]
+        focal_leny = cam_mtx[1,1]
+        focal_area = focal_lenx*focal_leny
+        
+        if localization_type == "nn":
+            while True:
+                #get metadata from camera
+                metadata = picam.capture_metadata()
+
+                #extract output tensor from metadata
+                objects = imx500.get_outputs(metadata)
+
+                if objects:
+                    #extract the boxes, confidence scores, and object types from detected objects
+                    boxes, scores, classes = objects[0], objects[1], objects[2]
+                    
+                    idx_biggest = -1
+                    biggest_box = 0
+                    #minimum confidence for an object to be considered
+                    threshold = 0.5
+                    for i in range(len(boxes)):
+                        if scores[i] > threshold:# and classes[i] == 0:
+                            area = boxes[i][2]*boxes[i][3]
+                            if area > biggest_box:
+                                biggest_box = area
+                                idx_biggest = i
+
+                    if idx_biggest >= 0:
+                        # extract normalized coordinates, confidence and object type
+                        # the camera is preset to portrait mode, so we extract the coords and dims this way
+                        y, x, h, w = boxes[idx_biggest]
+                        confidence = scores[idx_biggest]
+                        class_i = int(classes[idx_biggest])
+
+                        #get exact pixel location of object:
+                        width, height = 2028, 1520
+                        pixel_x = x * width
+                        pixel_y = y * height
+                        pixel_w = w * width
+                        pixel_h = h * height
+
+                        #get coordinates of middle of object
+                        u = float(pixel_x + (pixel_w/2))
+                        v = float(pixel_y + (pixel_h/2))
+                        w_coord = 1.0
+
+                        #create array with pixel coordinates and convert to camera's coordinates
+                        center_coords = np.array([u, v, w_coord], dtype=np.float64)
+                        camera_vec = cam_mtx_inv.dot(center_coords)
+                        camera_vec[0] = -camera_vec[0]
+
+                        #extract class name and dimensions from index
+                        category = categories[class_i][:-1]
+                        dimensions = dims[class_i]
+                        real_w = 1
+                        real_h = 1
+                        if '-' not in dimensions:
+                            real_w = float(dimensions[:4])
+                            real_h = float(dimensions[5:9])
+                            
+                        #print(real_w)
+                        #print(real_h)
+                        #compute distance based on IRL area of object
+                        pixel_area = pixel_w*pixel_h
+                        real_area = real_w*real_h
+
+                        distance = np.sqrt((real_area*focal_area)/float(pixel_area))
+
+                        #if real_area < 0.5:
+                        #   chirp.set_len(0.4)
+                        
+                        if real_area < 0.15:
+                            chirp.set_len(2)
+
+                        #print(f"len: {chirp.get_len()}, area: {real_area}")
+                        #compute horizontal and vertical angles
+                        azimuth = np.degrees(np.arctan2(camera_vec[0], 1)) * -1
+                        elevation = np.degrees(np.arctan2(camera_vec[1], 1)) * -1
+
+                        target = (float(azimuth), float(elevation), float(distance))
+
+                        print(f'Object of class {category} found with confidence {confidence:.2f}')
+                        #print(f'Camera vector: {camera_vec}')
+                        #print(f"Object at X: {x:.2f}, Y: {y:.2f} (Width: {pixel_w:.2f})")
+                        print(f'Azimuth: {target[0]}, Elevation: {target[1]}, Distance: {target[2]}')
+                        
+                        #send target point to pipeline for audio
+                        parent_conn.send(target)
+                
+                    print("-------------------------------------------")
+
+                time.sleep(0.01)
+        else:
+            lower_yellow = np.array([18, 64, 16])
+            upper_yellow = np.array([36, 255, 255])
+            
+            real_w = 0.35
+            real_h = 0.35
+            real_area = real_w*real_h
+            
+            while True:
+                img = picam.capture_array()
+                
+                #convert from BGR to HSV (hue, saturation, value) color space
+                hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+                
+                #create mask and kernel
+                yellow_mask = cv.inRange(hsv, lower_yellow, upper_yellow)
+                kernel = np.ones((13, 13), np.uint8)
+                
+                #clean up noise using morphology (erosion and dilation)
+                yellow_mask = cv.morphologyEx(yellow_mask, cv.MORPH_OPEN, kernel)
+                yellow_mask = cv.morphologyEx(yellow_mask, cv.MORPH_CLOSE, kernel)
+                
+                #minimum pixel area of object to consider (20x20 size)
+                MIN_AREA = 400
+
+                #find contours of the yellow parts
+                contours, _ = cv.findContours(yellow_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+                
+                #filter contours to only include those larger than 20x20 pixels
+                filtered_contours = []
+                for contour in contours:
+                    if cv.contourArea(contour) > MIN_AREA:
+                        filtered_contours.append(contour)
+                
+                #choose index of biggest bounding box
+                idx_biggest = -1
+                biggest_box = 0
+                for i in range(len(filtered_contours)):
+                    x, y, w, h = cv.boundingRect(filtered_contours[i])
+                    area = w*h
+                    if area > biggest_box:
+                        biggest_box = area
+                        idx_biggest = i
+                
+                #process only the largest box (nearest object)
+                if idx_biggest >= 0:
+                    x, y, w, h = cv.boundingRect(filtered_contours[idx_biggest])
+                    
+                    #get coordinates of middle of object
+                    u = float(x + (w/2))
+                    v = float(y + (h/2))
+                    w_coord = 1.0
+                    
+                    #create array with pixel coordinates and convert to camera's coordinates
+                    center_coords = np.array([u, v, w_coord], dtype=np.float64)
+                    camera_vec = cam_mtx_inv.dot(center_coords)
+                    
+                    pixel_area = w*h
+
+                    distance = np.sqrt((real_area*focal_area)/float(pixel_area))
+
+                    if real_area < 0.15:
+                        chirp.set_len(2)
+
+                    #print(f"len: {chirp.get_len()}, area: {real_area}")
+                    #compute horizontal and vertical angles
+                    azimuth = np.degrees(np.arctan2(camera_vec[0], 1)) * -1
+                    elevation = np.degrees(np.arctan2(camera_vec[1], 1)) * -1
+
+                    target = (float(azimuth), float(elevation), float(distance))
+
+                    print(f'An object of color yellow was found!')
+                    #print(f'Camera vector: {camera_vec}')
+                    #print(f"Object at X: {x:.2f}, Y: {y:.2f} (Width: {pixel_w:.2f})")
+                    print(f'Azimuth: {target[0]}, Elevation: {target[1]}, Distance: {target[2]}')
+                        
+                    #send target point to pipeline for audio
+                    parent_conn.send(target)
+                    
+                    os.remove(path+'/imagesForSeg/frame.jpg')
+                
+                print("-------------------------------------------")
+                    
+                time.sleep(0.01)
+            
+
+    except KeyboardInterrupt:
+        parent_conn.send(False)
+        p.join(timeout=2)
+
+        picam.stop()
+        picam.close()
